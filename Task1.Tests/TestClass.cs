d5a4b0465f6230144e474e3a20949f075daff0a1
Grosss <artemenko,nickolai@gmail.com>
2017-03-23 04:13:18 +0200

    Task1 (Euclid)

---
+++ Task1.Tests/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class GCDCalculatorTests
+    {
+        [TestCase(12, 8, ExpectedResult = 4)]
+        [TestCase(0, 12, ExpectedResult = 12)]
+        [TestCase(6, 6, ExpectedResult = 6)]
+        [TestCase(15, 18, ExpectedResult = 3)]
+        public int UseEuclideanAlgorithm_PassedTwoNumbers_ExpectedPositiveTest(int firstNumber, int secondNumber)
+        {
+            return GCDCalculator.UseEuclideanAlgorithm(firstNumber, secondNumber);
+        }
+
+        [TestCase(12, 8, ExpectedResult = 4)]
+        [TestCase(0, 12, ExpectedResult = 12)]
+        [TestCase(6, 6, ExpectedResult = 6)]
+        [TestCase(15, 18, ExpectedResult = 3)]
+        public int UseEuclideanAlgorithm_PassedTwoNumbersAndTime_ExpectedPositiveTest(int firstNumber, int secondNumber)
+        {
+            long time;
+
+            int actual = GCDCalculator.UseEuclideanAlgorithm(out time, firstNumber, secondNumber);
+            Debug.WriteLine($"Evaluating time is {time}ms");
+
+            return actual;
+        }
+    }
+}
---
+++ Task1/GCDCalculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task1
+{
+    public static class GCDCalculator
+    {
+        public static int UseEuclideanAlgorithm(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 && secondNumber == 0)
+                throw new ArgumentException();
+
+            int result = GetEuclideanGCD(firstNumber, secondNumber);
+            return result;
+        }
+
+        public static int UseEuclideanAlgorithm(out long timeInMilliseconds, int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 && secondNumber == 0)
+                throw new ArgumentException();
+
+            Stopwatch timer = new Stopwatch();
+            int result = GetEuclideanGCD(firstNumber, secondNumber);
+            timer.Stop();
+            timeInMilliseconds = timer.ElapsedMilliseconds;
+            return result;
+        }
+
+        public static int UseEuclideanAlgorithm(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber == 0 && secondNumber == 0 && thirdNumber == 0)
+                throw new ArgumentException();
+
+            int result = GetEuclideanGCD(GetEuclideanGCD(firstNumber, secondNumber), thirdNumber);
+            return result;
+        }
+
+        public static int UseEuclideanAlgorithm(out long timeInMilliseconds, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber == 0 && secondNumber == 0 && thirdNumber == 0)
+                throw new ArgumentException();
+
+            Stopwatch timer = new Stopwatch();
+            int result = GetEuclideanGCD(GetEuclideanGCD(firstNumber, secondNumber), thirdNumber);
+            timer.Stop();
+            timeInMilliseconds = timer.ElapsedMilliseconds;
+            return result;
+        }
+
+        public static int UseEuclideanAlgorithm(params int[] setOfNumbers)
+        {
+            if (setOfNumbers == null)
+                throw new ArgumentNullException();
+
+            if (setOfNumbers.Length < 2)
+                throw new ArgumentOutOfRangeException();
+
+            if (setOfNumbers.All(x => x == 0))
+                throw new ArgumentException();
+
+            int result = setOfNumbers[0];
+            for (int i = 1; i < setOfNumbers.Length; i++)
+            {
+                result = GetEuclideanGCD(setOfNumbers[i], result);
+            }
+            return result;
+        }
+
+        public static int UseEuclideanAlgorithm(out long timeInMilliseconds, params int[] setOfNumbers)
+        {
+            if (setOfNumbers == null)
+                throw new ArgumentNullException();
+
+            if (setOfNumbers.Length < 2)
+                throw new ArgumentOutOfRangeException();
+
+            if (setOfNumbers.All(x => x == 0))
+                throw new ArgumentException();
+
+            Stopwatch timer = new Stopwatch();
+            int result = setOfNumbers[0];
+            for (int i = 1; i < setOfNumbers.Length; i++)
+            {
+                result = GetEuclideanGCD(setOfNumbers[i], result);
+            }
+            
+            timer.Stop();
+            timeInMilliseconds = timer.ElapsedMilliseconds;
+            return result;
+        }
+
+        #region Private Methods
+        private static int GetEuclideanGCD(int a, int b)
+        {
+            int temp;
+            while (b != 0)
+            {
+                temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a);
+        }
+        #endregion
+    }
+}
